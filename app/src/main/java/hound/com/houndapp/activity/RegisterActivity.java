899b62b1faf0cfc740675a21258c6c9456fc4107
Diego Mansilla <dmansilla07@gmail.com>
2017-10-24 00:42:57 -0500

    Registration email fixed

--- app/src/main/java/hound/com/houndapp/activity/RegisterActivity.java
+++ app/src/main/java/hound/com/houndapp/activity/RegisterActivity.java
@@ -3,11 +3,13 @@ package hound.com.houndapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.stepstone.stepper.StepperLayout;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import hound.com.houndapp.R;
@@ -21,10 +23,20 @@ public class RegisterActivity extends AppCompatActivity {
 
     private StepperLayout stepperLayout;
 
+
+    @BindView(R.id.input_email_registro)
+    TextView email;
+
+    @BindView(R.id.input_password_registro)
+    TextView password;
+
+    FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.register_flow_step2);
         ButterKnife.bind(this);
         //stepperLayout = (StepperLayout) findViewById(R.id.register_step_1);
@@ -39,6 +51,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_register)
     public void initRegister(){
+        auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString());
         Intent intent = new Intent(RegisterActivity.this,RegisterFlowActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/hound/com/houndapp/activity/RegisterFlowActivity.java
+++ app/src/main/java/hound/com/houndapp/activity/RegisterFlowActivity.java
@@ -29,23 +29,15 @@ import hound.com.houndapp.R;
 
 public class RegisterFlowActivity extends AppCompatActivity {
 
-    @BindView(R.id.input_email_registro)
-    TextView email;
-
-    @BindView(R.id.input_password_registro)
-    TextView password;
-
 
     private int currentPos = 1;
 
     final private static int RESULT_TO_LOAD_IMG=1;
-    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        auth = FirebaseAuth.getInstance();
 
         setContentView(R.layout.activity_register_flow);
         ButterKnife.bind(this);
@@ -84,7 +76,7 @@ public class RegisterFlowActivity extends AppCompatActivity {
         oldView.setVisibility(View.GONE);
         newView.setVisibility(View.VISIBLE);
         if(currentPos==9){
-            auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString());
+
             View view = findViewById(R.id.prevActionBtn);
             view.setVisibility(View.GONE);
             Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show();
